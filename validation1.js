ccd73f7c2868ad6b9c6dd371451afecce6b96375
diegort <diegort@iastate.edu>
2019-02-09 21:03:09 -0600

    some tweaking, text checki9ng is done

--- validation1.js
+++ validation1.js
@@ -1,14 +1,27 @@
+/**
+ * Simple Javascript file containing some basic functions to
+ * control the HTML validation form with some images.
+ */
+
+var imgYes = "correct.png";
+var imgNo = "wrong.png";
 
 function submitForm() {
-    validateText('fname-');
-    validateText('lname-');
+    checkFilled('fname-');
+    checkFilled('lname-');
+    checkSelected('gender-');
 }
 
-function validateText(name) {
+function checkFilled(name) {
+    document.getElementById(name + 'i').style.display="block";
     if(document.getElementById(name + 'f').value){
-        document.getElementById(name + 'i').style.display="block";
+        document.getElementById(name + 'i').src=imgYes;
     }
     else{
-        document.getElementById(name + 'i').style.display="none";
+        document.getElementById(name + 'i').src=imgNo;
     }
+}
+
+function checkSelected(name){
+
 }
