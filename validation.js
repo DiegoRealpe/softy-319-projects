bc7dcadd9b952341ea2e7a9ef9617d27d9cdb4ea
diegort <diegort@iastate.edu>
2019-02-09 21:51:20 -0600

    added second html

--- validation.js
+++ validation.js
@@ -7,10 +7,10 @@ var imgYes = "correct.png";
 var imgNo = "wrong.png";
 var formIdArr = ['fname-', 'lname-', 'gender-', 'state-'];
 
-function submitForm() {
+function checkBasic() {
     let redirect = true;
     formIdArr.forEach(id => {
-        if(!checkFilled(id)){
+        if(!isFilled(id)){
             redirect = false;
         }
     });
@@ -19,7 +19,7 @@ function submitForm() {
     }
 }
 
-function checkFilled(name) {
+function isFilled(name) {
     document.getElementById(name + 'i').style.display="block";
     if(document.getElementById(name + 'f').value){
         document.getElementById(name + 'i').src=imgYes;
