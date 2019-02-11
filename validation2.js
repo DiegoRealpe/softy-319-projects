2397f6ba157f89c5be74470f2bd11924da333770
diegort <diegort@iastate.edu>
2019-02-10 21:45:07 -0600

    Forgot to change display lol

--- validation2.js
+++ validation2.js
@@ -5,22 +5,20 @@
 
 var imgYes = "correct.png";
 var imgNo = "wrong.png";
+
 const phoneRegEx = /[0-9]{3}[-]?[0-9]{3}[-]?[0-9]{4}/;
-const emailRegEx = /[a-zA-Z0-9]{3,}[@]{1}[a-zA-Z0-9]{3,}[\.]{1}[a-zA-Z0-9]{2,3}/
+const emailRegEx = /[a-zA-Z0-9]{3,}[@]{1}[a-zA-Z0-9]{3,}[\.]{1}[a-zA-Z0-9]{2,3}/;
 const addressRegEx = /[a-zA-Z]{2,}[,]{1}[A-Z]{2}/;
 
 function submitAll(){
-    console.log("function executing");
     let email = emailCheck();
     let phone  = phoneCheck();
     let addr = addressCheck();
-    console.log(email);
-    console.log(phone);
-    console.log(addr);
 }
 
 function emailCheck(){
     let input = document.getElementById('email-f').value;
+    document.getElementById('email-i').style.display="block";
     if(emailRegEx.test(input)){
         document.getElementById('email-i').src=imgYes;
         return true;
@@ -31,6 +29,7 @@ function emailCheck(){
 
 function phoneCheck(){
     let input = document.getElementById('phone-f').value;
+    document.getElementById('phone-i').style.display="block";
     if(phoneRegEx.test(input)){
         document.getElementById('phone-i').src=imgYes;
         return true;
@@ -41,6 +40,7 @@ function phoneCheck(){
 
 function addressCheck(){
     let input = document.getElementById('address-f').value;
+    document.getElementById('address-i').style.display="block";
     if(addressRegEx.test(input)){
         document.getElementById('address-i').src=imgYes;
         return true;  
