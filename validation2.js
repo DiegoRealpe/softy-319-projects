bdb06d1337c35a69916605da3d6d9ee25d728ac7
diegort <diegort@iastate.edu>
2019-02-10 21:30:58 -0600

    Computer froze for 5 seconds, panic commit

--- validation2.js
+++ validation2.js
@@ -10,19 +10,41 @@ const emailRegEx = /[a-zA-Z0-9]{3,}[@]{1}[a-zA-Z0-9]{3,}[\.]{1}[a-zA-Z0-9]{2,3}/
 const addressRegEx = /[a-zA-Z]{2,}[,]{1}[A-Z]{2}/;
 
 function submitAll(){
-    emailCheck();
-    phoneCheck();
-    addressCheck();
+    console.log("function executing");
+    let email = emailCheck();
+    let phone  = phoneCheck();
+    let addr = addressCheck();
+    console.log(email);
+    console.log(phone);
+    console.log(addr);
 }
 
 function emailCheck(){
-
+    let input = document.getElementById('email-f').value;
+    if(emailRegEx.test(input)){
+        document.getElementById('email-i').src=imgYes;
+        return true;
+    }
+    document.getElementById('email-i').src=imgNo;
+    return false;
 }
 
 function phoneCheck(){
-
+    let input = document.getElementById('phone-f').value;
+    if(phoneRegEx.test(input)){
+        document.getElementById('phone-i').src=imgYes;
+        return true;
+    }
+    document.getElementById('phone-i').src=imgNo;
+    return false;
 }
 
 function addressCheck(){
-    
+    let input = document.getElementById('address-f').value;
+    if(addressRegEx.test(input)){
+        document.getElementById('address-i').src=imgYes;
+        return true;  
+    }
+    document.getElementById('address-i').src=imgNo;
+    return false;
 }
