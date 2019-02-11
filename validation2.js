81f562919fe0f13d223b25185e86a8ea9cd554da
diegort <diegort@iastate.edu>
2019-02-10 21:06:50 -0600

    renaming and setting up RegExps for validation

---
+++ validation2.js
@@ -0,0 +1,28 @@
+/**
+ * Simple Javascript file containing the second part
+ * of the verification project
+ */
+
+var imgYes = "correct.png";
+var imgNo = "wrong.png";
+const phoneRegEx = /[0-9]{3}[-]?[0-9]{3}[-]?[0-9]{4}/;
+const emailRegEx = /[a-zA-Z0-9]{3,}[@]{1}[a-zA-Z0-9]{3,}[\.]{1}[a-zA-Z0-9]{2,3}/
+const addressRegEx = /[a-zA-Z]{2,}[,]{1}[A-Z]{2}/;
+
+function submitAll(){
+    emailCheck();
+    phoneCheck();
+    addressCheck();
+}
+
+function emailCheck(){
+
+}
+
+function phoneCheck(){
+
+}
+
+function addressCheck(){
+    
+}
