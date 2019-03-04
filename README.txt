4c7a32793f4fbc053f5025e2d82002491e6e3f5c
diegort <diegort@iastate.edu>
2019-03-04 00:05:50 -0600

    finished last tweaks

---
+++ README.txt
@@ -0,0 +1,15 @@
+//README!
+
+The two exercises are nopthing complex and anyone can run them with little effort
+
+The first exercise only requirement is that you keep all 3 files
+snake.html, snake.css and snake.js in the same level directory because
+that is where the html is gonna look for them.
+
+One is the actual facets of programming and the other gives it a little order and color
+
+The second exercise uses a library called "Inquirer.js" that is in charge of reading input
+from the console and can be installed using the command => "npm install inquirer"
+Plase install Inquirer.js and afterwards run the exercise by typing => "node hw3.js"
+
+That is all folks!
--- hw3.js
+++ hw3.js
@@ -59,5 +59,47 @@ var questions = [{
 
 console.log("Please input 3 numbers");
 inquirer.prompt(questions).then(answers => {
-    console.log(answers.first);
+
+    //calculate the factorial of the first number
+    let fact = 1;
+    for(let i = answers.first; i > 0; --i){
+        fact *= i;
+    }
+    console.log("Factorial of 1st number is = " + fact);
+
+    //add digits
+    let digits = 0;
+    for(let i = 0; i < answers.second.length; ++i){
+        digits += Number(answers.second[i]);
+    }
+    console.log("Sum of all secound number's digits = " + digits);
+
+    //inversing number
+    let inverse = "";
+    for(let i = answers.third.length - 1; i >= 0 ; --i){
+        inverse += answers.third[i];
+    }
+    console.log("The inverse of the 3rd number is = " + inverse);
+
+    //palindrome check
+    let palindrome = true;
+    let start = 0;
+    let end = answers.fourth.length - 1; 
+    while(start <= (answers.fourth.length/2) &&  end >= (answers.fourth.length/2)){
+        if(answers.fourth[start] != answers.fourth[end]){
+            palindrome = false;
+            break;
+        }
+        ++start;
+        --end;
+    }
+    console.log("Is the fourth number a palindrome? (T/F) " + palindrome);
+    if(palindrome){
+        console.log("True");
+    }
+    else{
+        console.log("False");
+    }
+
+
 })
