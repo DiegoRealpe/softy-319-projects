91e4417f96cc7330400b8adb28a8b3ac915a278d
diegort <diegort@iastate.edu>
2019-03-06 22:31:31 -0600

    small changes

--- TicTacToe/src/gui/TicTacToeGame.java
+++ TicTacToe/src/gui/TicTacToeGame.java
@@ -1,20 +1,19 @@
 package gui;
 
-enum Symbol{
-	EMPTY, CIRCLE,CROSS;
+enum Symbol {
+	EMPTY, CIRCLE, CROSS;
 }
 
 public class TicTacToeGame {
-	
+
 	private Symbol TileGrid[][] = new Symbol[3][3];
-	
-	//Cross starts first
+
+	// Cross starts first
 	private Symbol CurrentTurn = Symbol.CROSS;
 
 	private int CrossScore;
 	private int CircleScore;
 	private boolean Over;
-	
 
 	public TicTacToeGame() {
 		for (int i = 0; i < TileGrid.length; i++) {
@@ -23,46 +22,46 @@ public class TicTacToeGame {
 			}
 		}
 	}
-	
+
 	public void claimTile(int row, int col) {
-		if(row > 2 || col > 2 || Over) {
+		if (row > 2 || col > 2 || Over) {
 			return;
 		}
-		if(TileGrid[row][col] == Symbol.EMPTY) {
+		if (TileGrid[row][col] == Symbol.EMPTY) {
 			TileGrid[row][col] = CurrentTurn;
 			checkOver();
-			switchTurn();
+			if (!Over) {
+				switchTurn();
+			}
 		}
 	}
-	
+
 	public void checkOver() {
-		//Couldn't really think of better optimized version of this
-		//Check both rows and cols
+		// Couldn't really think of better optimized version of this
+		// Check both rows and cols
 		int rowRun, colRun, diaRun = 0;
 		for (int i = 0; i < TileGrid.length; i++) {
-			if(TileGrid[i][0] == TileGrid[i][1] && TileGrid[i][1] == TileGrid[i][2]) {
-				//person who had turn is always gonna win
+			if ((TileGrid[i][0] == TileGrid[i][1] && TileGrid[i][1] == TileGrid[i][2])
+					|| (TileGrid[0][i] == TileGrid[1][i] && TileGrid[1][i] == TileGrid[2][i])) {
+				//Whoever's turn it is would win
 				Over = true;
-			}
-			if(TileGrid[0][i] == TileGrid[1][i] && TileGrid[1][i] == TileGrid[2][i]) {
-						
+				
 			}
 		}
 	}
-	
+
 	public void switchTurn() {
-		CurrentTurn = (CurrentTurn == Symbol.CIRCLE)? (Symbol.CROSS) : (Symbol.CIRCLE);
+		CurrentTurn = (CurrentTurn == Symbol.CIRCLE) ? (Symbol.CROSS) : (Symbol.CIRCLE);
 	}
-	
+
 	public Symbol getTile(int row, int col) {
 		return TileGrid[row][col];
 	}
-	
+
 	public int getScore(Symbol Team) {
-		if(Team == Symbol.CIRCLE) {
+		if (Team == Symbol.CIRCLE) {
 			return CircleScore;
-		}
-		else if(Team == Symbol.CROSS) {
+		} else if (Team == Symbol.CROSS) {
 			return CrossScore;
 		}
 		return -1;
